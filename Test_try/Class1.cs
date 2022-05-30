7c98265d399be518f1da9f60356254705695e371
user0ren <88614567+user0ren@users.noreply.github.com>
2022-05-30 12:20:23 +0300

    Add files via upload

---
+++ Test_try/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Test_try
+{
+    public class Class1
+    {
+        public static string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations,
+            TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+        {
+            int duration = 0;
+            foreach (int i in durations)
+                duration += i;
+
+            TimeSpan consultationTimeSpan = new TimeSpan(0, consultationTime, 0);
+            int pusto = 0;
+            for (int i = 1; i < startTimes.Length; i++)
+            {
+                TimeSpan durationTimeSpan = new TimeSpan(0, durations[i], 0);
+                if (startTimes[i] - (startTimes[i - 1] + durationTimeSpan) < consultationTimeSpan)
+                    pusto += 1;
+            }
+
+            int nomer = 0;
+            int dlina = (endWorkingTime.Hours*60 + endWorkingTime.Minutes - beginWorkingTime.Hours * 60 + 
+                beginWorkingTime.Minutes - duration) / consultationTime - pusto;
+            string[] str = new string[dlina];
+
+            TimeSpan now = beginWorkingTime;
+            TimeSpan now_1 = new TimeSpan(now.Hours, now.Minutes + consultationTime, now.Seconds);
+
+            for (int i = 0; now < endWorkingTime; i++)
+            {
+                if (nomer <= startTimes.Length - 1 && now > (startTimes[nomer] - consultationTimeSpan))
+                {
+                    now_1 = new TimeSpan(startTimes[nomer].Hours, startTimes[nomer].Minutes + durations[nomer], startTimes[nomer].Seconds);
+                    nomer += 1;
+                }
+                else
+                {
+                    str[i - nomer] = now.ToString() + " - " + now_1.ToString();
+                }                
+                now = now_1;
+                now_1 = new TimeSpan(now.Hours,  now.Minutes + consultationTime, now.Seconds);
+            }
+
+            return str;
+        }
+    }
+}
---
+++ Test_try/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Test_try
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            TimeSpan[] startTimes = 
+                { 
+                new TimeSpan(10, 0, 0),
+                new TimeSpan(11, 0, 0),
+                new TimeSpan(15, 0, 0),
+                new TimeSpan(15, 30, 0),
+                new TimeSpan(16, 50, 0)
+                };
+
+
+            int[] durations = { 60, 30, 10, 10, 40 };
+
+            TimeSpan beginWorkingTime = new TimeSpan(8, 0, 0);
+            TimeSpan endWorkingTime = new TimeSpan(18, 0, 0);
+            int consultationTime = 30;
+
+            string[] str = Class1.AvailablePeriods(startTimes, durations, beginWorkingTime,
+                endWorkingTime, consultationTime);
+
+            Console.WriteLine("Str:");
+            foreach (string i in str)
+                Console.WriteLine(i);
+        }
+    }
+}
